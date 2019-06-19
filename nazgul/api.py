a97dbb0c6269d82f532c943f0746efc9c9455aff
Aaron Amanuel <amanuel.aaron1@gmail.com>
2019-06-19 13:36:32 -0700

    bouncerscript test runs against nazgul

--- nazgul/api.py
+++ nazgul/api.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, request, Response, Blueprint
 from nazgul.mysql_model import MySQLModel, ModelError
 import nazgul.xmlrenderer as xmlrenderer
 import urllib.parse
-import ast
 import os
 
 bp = Blueprint("api", __name__, url_prefix="/api")
@@ -17,7 +16,7 @@ def index():
     return "Nazgul API"
 
 
-@bp.route("/location_show", methods=["GET"])
+@bp.route("/location_show/", methods=["GET"])
 def location_show():
     xml = xmlrenderer.XMLRenderer()
 
@@ -40,7 +39,7 @@ def location_show():
     return Response(data, mimetype="text/xml"), status
 
 
-@bp.route("/location_add", methods=["POST"])
+@bp.route("/location_add/", methods=["POST"])
 def location_add():
     xml = xmlrenderer.XMLRenderer()
 
@@ -69,7 +68,7 @@ def location_add():
     return Response(data, mimetype="text/xml"), status
 
 
-@bp.route("/location_modify", methods=["POST"])
+@bp.route("/location_modify/", methods=["POST"])
 def location_modify():
     xml = xmlrenderer.XMLRenderer()
 
@@ -92,7 +91,7 @@ def location_modify():
     return Response(data, mimetype="text/xml"), status
 
 
-@bp.route("/location_delete", methods=["POST"])
+@bp.route("/location_delete/", methods=["POST"])
 def location_delete():
     xml = xmlrenderer.XMLRenderer()
 
@@ -114,7 +113,7 @@ def location_delete():
     return Response(data, mimetype="text/xml"), status
 
 
-@bp.route("/product_show", methods=["GET"])
+@bp.route("/product_show/", methods=["GET"])
 def product_show():
     xml = xmlrenderer.XMLRenderer()
 
@@ -132,13 +131,12 @@ def product_show():
     return Response(data, mimetype="text/xml"), status
 
 
-@bp.route("/product_add", methods=["POST"])
+@bp.route("/product_add/", methods=["POST"])
 def product_add():
     xml = xmlrenderer.XMLRenderer()
 
     product = request.form.get("product", None)
-    lang_in = request.form.get("languages", None)
-    languages = ast.literal_eval(urllib.parse.unquote(lang_in))
+    languages = request.form.getlist("languages", None)
     ssl_only = request.form.get("ssl_only") == "True"
     try:
         res = msm.product_add(product, languages, ssl_only)
@@ -155,7 +153,7 @@ def product_add():
     return Response(data, mimetype="text/xml"), status
 
 
-@bp.route("/product_delete", methods=["POST"])
+@bp.route("/product_delete/", methods=["POST"])
 def product_delete():
     xml = xmlrenderer.XMLRenderer()
 
@@ -179,13 +177,12 @@ def product_delete():
     return Response(data, mimetype="text/xml"), status
 
 
-@bp.route("/product_language_add", methods=["POST"])
+@bp.route("/product_language_add/", methods=["POST"])
 def product_language_add():
     xml = xmlrenderer.XMLRenderer()
 
     product = request.form.get("product", None)
-    lang_in = request.form.get("languages", None)
-    languages = ast.literal_eval(urllib.parse.unquote(lang_in))
+    languages = request.form.getlist("languages", None)
     try:
         res = msm.product_language_add(product, languages)
         xml.prepare_products(res)
@@ -200,13 +197,12 @@ def product_language_add():
     return Response(data, mimetype="text/xml"), status
 
 
-@bp.route("/product_language_delete", methods=["POST"])
+@bp.route("/product_language_delete/", methods=["POST"])
 def product_language_delete():
     xml = xmlrenderer.XMLRenderer()
 
     product = request.form.get("product", None)
-    lang_in = request.form.get("languages", None)
-    languages = ast.literal_eval(urllib.parse.unquote(lang_in))
+    languages = request.form.getlist("languages", None)
     try:
         res = msm.product_language_delete(product, languages)
         data = xml.success(res)
@@ -220,7 +216,7 @@ def product_language_delete():
     return Response(data, mimetype="text/xml"), status
 
 
-@bp.route("/mirror_list", methods=["GET"])
+@bp.route("/mirror_list/", methods=["GET"])
 def mirror_list():
     xml = xmlrenderer.XMLRenderer()
 
@@ -230,7 +226,7 @@ def mirror_list():
     return Response(data, mimetype="text/xml"), 400
 
 
-@bp.route("/uptake", methods=["GET"])
+@bp.route("/uptake/", methods=["GET"])
 def uptake():
     xml = xmlrenderer.XMLRenderer()
 
@@ -256,7 +252,7 @@ def uptake():
     return Response(data, mimetype="text/xml"), status
 
 
-@bp.route("/create_update_alias", methods=["POST"])
+@bp.route("/create_update_alias/", methods=["POST"])
 def create_update_alias():
     xml = xmlrenderer.XMLRenderer()
 
--- tests/test_api.py
+++ tests/test_api.py
@@ -9,44 +9,44 @@ msm._reset_db()
 
 
 def test_location_show_exact_match(client):
-    rv = client.get("/api/location_show?product=Firefox")
+    rv = client.get("/api/location_show/?product=Firefox")
     expected = b'<?xml version="1.0" encoding="utf-8"?><locations><product id="1" name="Firefox"><location id="1" os="win64">/firefox/releases/39.0/win64/:lang/Firefox%20Setup%2039.0.exe</location><location id="2" os="osx">/firefox/releases/39.0/mac/:lang/Firefox%2039.0.dmg</location><location id="3" os="win">/firefox/releases/39.0/win32/:lang/Firefox%20Setup%2039.0.exe</location></product></locations>'
     assert expected == rv.data
 
 
 def test_location_show_exact_no_match(client):
-    rv = client.get("/api/location_show?product=NoMatch")
+    rv = client.get("/api/location_show/?product=NoMatch")
     expected = b'<?xml version="1.0" encoding="utf-8"?>'
     assert expected == rv.data
 
 
 def test_location_show_fuzzy_match(client):
-    rv = client.get("/api/location_show?product=Firefox&fuzzy=True")
+    rv = client.get("/api/location_show/?product=Firefox&fuzzy=True")
     expected = b'<?xml version="1.0" encoding="utf-8"?><locations><product id="1" name="Firefox"><location id="1" os="win64">/firefox/releases/39.0/win64/:lang/Firefox%20Setup%2039.0.exe</location><location id="2" os="osx">/firefox/releases/39.0/mac/:lang/Firefox%2039.0.dmg</location><location id="3" os="win">/firefox/releases/39.0/win32/:lang/Firefox%20Setup%2039.0.exe</location></product><product id="3" name="Firefox-43.0.1-SSL"><location id="7" os="win64">/firefox/releases/43.0.1/win64/:lang/Firefox%20Setup%2043.0.1.exe</location><location id="8" os="osx">/firefox/releases/43.0.1/mac/:lang/Firefox%2043.0.1.dmg</location><location id="9" os="win">/firefox/releases/43.0.1/win32/:lang/Firefox%20Setup%2043.0.1.exe</location></product><product id="2" name="Firefox-SSL"><location id="4" os="win64">/firefox/releases/39.0/win64/:lang/Firefox%20Setup%2039.0.exe</location><location id="5" os="osx">/firefox/releases/39.0/mac/:lang/Firefox%2039.0.dmg</location><location id="6" os="win">/firefox/releases/39.0/win32/:lang/Firefox%20Setup%2039.0.exe</location></product></locations>'
     assert expected == rv.data
 
 
 def test_location_show_fuzzy_no_match(client):
-    rv = client.get("/api/location_show?product=NoMatch&fuzzy=True")
+    rv = client.get("/api/location_show/?product=NoMatch&fuzzy=True")
     expected = b'<?xml version="1.0" encoding="utf-8"?>'
     assert expected == rv.data
 
 
 def test_uptake(client):
-    rv = client.get("api/uptake?product=Firefox&os=osx&fuzzy=True")
+    rv = client.get("api/uptake/?product=Firefox&os=osx&fuzzy=True")
     expected = b'<?xml version="1.0" encoding="utf-8"?><mirror_uptake><item><product>Firefox</product><os>osx</os><available>2000000</available><total>2000000</total></item><item><product>Firefox-43.0.1-SSL</product><os>osx</os><available>2000000</available><total>2000000</total></item><item><product>Firefox-SSL</product><os>osx</os><available>2000000</available><total>2000000</total></item></mirror_uptake>'
     assert expected == rv.data
 
 
 def test_mirror_list(client):
-    rv = client.get("api/mirror_list")
+    rv = client.get("api/mirror_list/")
     expected = b'<?xml version="1.0" encoding="utf-8"?><mirrors><mirror baseurl="http://download-installer.cdn.mozilla.net/pub"/><mirror baseurl="https://download-installer.cdn.mozilla.net/pub"/></mirrors>'
     assert expected == rv.data
 
 
 def test_location_add_product_not_found(client):
     rv = client.post(
-        "api/location_add",
+        "api/location_add/",
         data=dict(product="FakeProduct", path="/test_path", os="osx"),
     )
     expected = b'<?xml version="1.0" encoding="utf-8"?><error number="105">FAILED: \'FakeProduct\' does not exist</error>'
@@ -55,7 +55,7 @@ def test_location_add_product_not_found(client):
 
 def test_location_add_os_not_found(client):
     rv = client.post(
-        "api/location_add", data=dict(product="Firefox", path="/test_path", os="fake")
+        "api/location_add/", data=dict(product="Firefox", path="/test_path", os="fake")
     )
     expected = b'<?xml version="1.0" encoding="utf-8"?><error number="106">FAILED: \'fake\' does not exist</error>'
     assert expected == rv.data
@@ -63,7 +63,7 @@ def test_location_add_os_not_found(client):
 
 def test_location_add_location_exists(client):
     rv = client.post(
-        "api/location_add", data=dict(product="Firefox", path="/test_path", os="osx")
+        "api/location_add/", data=dict(product="Firefox", path="/test_path", os="osx")
     )
     expected = b'<?xml version="1.0" encoding="utf-8"?><error number="104">The specified location already exists.</error>'
     assert expected == rv.data
@@ -71,7 +71,7 @@ def test_location_add_location_exists(client):
 
 def test_location_add(client):
     rv = client.post(
-        "api/location_add",
+        "api/location_add/",
         data=dict(product="AaronProduct", path="/test_path", os="win64"),
     )
     location_exits, location_id = msm.location_exists("win64", "AaronProduct")
@@ -86,19 +86,19 @@ def test_location_add(client):
 
 
 def test_location_delete_location_not_found(client):
-    rv = client.post("api/location_delete", data=dict(location_id=0))
+    rv = client.post("api/location_delete/", data=dict(location_id=0))
     expected = b'<?xml version="1.0" encoding="utf-8"?><error number="102">No location found.</error>'
     assert expected == rv.data
 
 
 def test_location_delete_location_input_missing(client):
-    rv = client.post("api/location_delete", data=dict(not_loc_id="fakeloc"))
+    rv = client.post("api/location_delete/", data=dict(not_loc_id="fakeloc"))
     expected = b'<?xml version="1.0" encoding="utf-8"?><error number="101">location_id is required.</error>'
     assert expected == rv.data
 
 
 def test_location_delete(client):
-    rv = client.post("api/location_delete", data=dict(location_id=23194))
+    rv = client.post("api/location_delete/", data=dict(location_id=23194))
     location_exists, location_id = msm.location_exists("osx", "AaronProduct")
     msm._reset_db()
     if not location_exists:
@@ -111,7 +111,7 @@ def test_location_delete(client):
 
 def test_location_modify_invalid_product(client):
     rv = client.post(
-        "api/location_modify",
+        "api/location_modify/",
         data=dict(product="FakeProduct", os="osx", path="/newpath"),
     )
     expected = b'<?xml version="1.0" encoding="utf-8"?><error number="105">FAILED: \'FakeProduct\' does not exist</error>'
@@ -120,7 +120,7 @@ def test_location_modify_invalid_product(client):
 
 def test_location_modify_invalid_os(client):
     rv = client.post(
-        "api/location_modify",
+        "api/location_modify/",
         data=dict(product="Firefox", os="fakeos", path="/newpath"),
     )
     expected = b'<?xml version="1.0" encoding="utf-8"?><error number="106">FAILED: \'fakeos\' does not exist</error>'
@@ -129,7 +129,7 @@ def test_location_modify_invalid_os(client):
 
 def test_location_modify_invalid_location(client):
     rv = client.post(
-        "api/location_modify",
+        "api/location_modify/",
         data=dict(product="AaronProduct", os="win", path="/newpath"),
     )
     expected = b'<?xml version="1.0" encoding="utf-8"?><error number="104">FAILED: location \'AaronProduct\' on OS \'win\' does not exist</error>'
@@ -138,7 +138,7 @@ def test_location_modify_invalid_location(client):
 
 def test_location_modify(client):
     rv = client.post(
-        "api/location_modify",
+        "api/location_modify/",
         data=dict(product="AaronProduct", os="osx", path="/newpath"),
     )
     expected = b'<?xml version="1.0" encoding="utf-8"?><locations><product id="4556" name="AaronProduct"><location id="23194" os="osx">/newpath</location></product></locations>'
@@ -147,32 +147,32 @@ def test_location_modify(client):
 
 
 def test_product_show_exact_match(client):
-    rv = client.get("/api/product_show?product=Firefox")
+    rv = client.get("/api/product_show/?product=Firefox")
     expected = b'<?xml version="1.0" encoding="utf-8"?><products><product id="1" name="Firefox"><language locale="en-GB"/><language locale="en-US"/></product></products>'
     assert expected == rv.data
 
 
 def test_product_show_exact_no_match(client):
-    rv = client.get("/api/product_show?product=NoMatch")
+    rv = client.get("/api/product_show/?product=NoMatch")
     expected = b'<?xml version="1.0" encoding="utf-8"?><products/>'
     assert expected == rv.data
 
 
 def test_product_show_fuzzy_match(client):
-    rv = client.get("/api/product_show?product=Firefox&fuzzy=True")
+    rv = client.get("/api/product_show/?product=Firefox&fuzzy=True")
     expected = b'<?xml version="1.0" encoding="utf-8"?><products><product id="1" name="Firefox"><language locale="en-GB"/><language locale="en-US"/></product><product id="3" name="Firefox-43.0.1-SSL"><language locale="en-GB"/><language locale="en-US"/></product><product id="2" name="Firefox-SSL"><language locale="en-GB"/><language locale="en-US"/></product></products>'
     assert expected == rv.data
 
 
 def test_product_show_fuzzy_no_match(client):
-    rv = client.get("/api/product_show?product=NoMatch&fuzzy=True")
+    rv = client.get("/api/product_show/?product=NoMatch&fuzzy=True")
     expected = b'<?xml version="1.0" encoding="utf-8"?><products/>'
     assert expected == rv.data
 
 
 def test_product_add_no_match(client):
     rv = client.post(
-        "api/product_add",
+        "api/product_add/",
         data=dict(product="AaronProduct", ssl_only="osx", languages={"en-GB"}),
     )
     expected = b'<?xml version="1.0" encoding="utf-8"?><error number="104">product already exists.</error>'
@@ -181,12 +181,8 @@ def test_product_add_no_match(client):
 
 def test_product_add(client):
     rv = client.post(
-        "api/product_add",
-        data={
-            "product": "Fake",
-            "ssl_only": "True",
-            "languages": "%5B%27en-CA%27%2C%27en-US%27%5D",
-        },
+        "api/product_add/",
+        data={"product": "Fake", "ssl_only": "True", "languages": ["en-CA", "en-US"]},
     )
     expected = b'<?xml version="1.0" encoding="utf-8"?><products><product id="4563" name="Fake"><language locale="en-CA"/><language locale="en-US"/></product></products>'
     msm._reset_db()
@@ -194,20 +190,20 @@ def test_product_add(client):
 
 
 def test_product_delete_id(client):
-    rv = client.post("api/product_delete", data={"product_id": 4556})
+    rv = client.post("api/product_delete/", data={"product_id": 4556})
     expected = b'<?xml version="1.0" encoding="utf-8"?><success>SUCCESS: product has been deleted</success>'
     msm._reset_db()
     assert expected == rv.data
 
 
 def test_product_delete_name_no_match(client):
-    rv = client.post("api/product_delete", data={"product": "Fake"})
+    rv = client.post("api/product_delete/", data={"product": "Fake"})
     expected = b'<?xml version="1.0" encoding="utf-8"?><error number="102">No product found.</error>'
     assert expected == rv.data
 
 
 def test_product_delete_name(client):
-    rv = client.post("api/product_delete", data={"product": "AaronProduct"})
+    rv = client.post("api/product_delete/", data={"product": "AaronProduct"})
     expected = b'<?xml version="1.0" encoding="utf-8"?><success>SUCCESS: product has been deleted</success>'
     msm._reset_db()
     assert expected == rv.data
@@ -215,11 +211,8 @@ def test_product_delete_name(client):
 
 def test_product_language_add(client):
     rv = client.post(
-        "api/product_language_add",
-        data={
-            "product": "AaronProduct",
-            "languages": "%5B%27fr-FR%27%2C%20%27es-SP%27%5D",
-        },
+        "api/product_language_add/",
+        data={"product": "AaronProduct", "languages": ["fr-FR", "es-SP"]},
     )
     expected = b'<?xml version="1.0" encoding="utf-8"?><products><product id="4556" name="AaronProduct"><language locale="en-CA"/><language locale="en-GB"/><language locale="en-US"/><language locale="es-SP"/><language locale="fr-FR"/></product></products>'
     msm._reset_db()
@@ -228,8 +221,8 @@ def test_product_language_add(client):
 
 def test_product_language_add_no_match(client):
     rv = client.post(
-        "api/product_language_add",
-        data={"product": "Fake", "languages": "%5B%27fr-FR%27%2C%20%27es-SP%27%5D"},
+        "api/product_language_add/",
+        data={"product": "Fake", "languages": ["fr-FR", "es-SP"]},
     )
     expected = b'<?xml version="1.0" encoding="utf-8"?><error number="102">Product not found.</error>'
     assert expected == rv.data
@@ -237,11 +230,8 @@ def test_product_language_add_no_match(client):
 
 def test_product_language_delete(client):
     rv = client.post(
-        "api/product_language_delete",
-        data={
-            "product": "AaronProduct",
-            "languages": "%5B%27en-GB%27%2C%20%27en-US%27%5D",
-        },
+        "api/product_language_delete/",
+        data={"product": "AaronProduct", "languages": ["en-GB", "en-US"]},
     )
     expected = b'<?xml version="1.0" encoding="utf-8"?><success>SUCCESS: language has been deleted</success>'
     msm._reset_db()
@@ -250,8 +240,8 @@ def test_product_language_delete(client):
 
 def test_product_language_delete_star(client):
     rv = client.post(
-        "api/product_language_delete",
-        data={"product": "AaronProduct", "languages": "%5B%27*%27%5D"},
+        "api/product_language_delete/",
+        data={"product": "AaronProduct", "languages": ["*"]},
     )
     expected = b'<?xml version="1.0" encoding="utf-8"?><success>SUCCESS: language has been deleted</success>'
     msm._reset_db()
@@ -260,8 +250,8 @@ def test_product_language_delete_star(client):
 
 def test_product_language_delete_no_match(client):
     rv = client.post(
-        "api/product_language_delete",
-        data={"product": "Fake", "languages": "%5B%27fr-FR%27%2C%20%27es-SP%27%5D"},
+        "api/product_language_delete/",
+        data={"product": "Fake", "languages": ["fr-FR", "es-SP"]},
     )
     expected = b'<?xml version="1.0" encoding="utf-8"?><error number="102">Product not found.</error>'
     assert expected == rv.data
@@ -269,7 +259,7 @@ def test_product_language_delete_no_match(client):
 
 def test_create_update_alias_no_product_match(client):
     rv = client.post(
-        "api/create_update_alias",
+        "api/create_update_alias/",
         data={"alias": "aaron-product", "related_product": "FakeProduct"},
     )
     expected = b'<?xml version="1.0" encoding="utf-8"?><error number="103">You must specify a valid product to match with an alias</error>'
@@ -278,7 +268,7 @@ def test_create_update_alias_no_product_match(client):
 
 def test_create_update_alias_alias_product_name_collision(client):
     rv = client.post(
-        "api/create_update_alias",
+        "api/create_update_alias/",
         data={"alias": "Firefox", "related_product": "AaronProduct"},
     )
     expected = b'<?xml version="1.0" encoding="utf-8"?><error number="104">You cannot create an alias with the same name as a product</error>'
@@ -287,7 +277,7 @@ def test_create_update_alias_alias_product_name_collision(client):
 
 def test_create_update_alias(client):
     rv = client.post(
-        "api/create_update_alias",
+        "api/create_update_alias/",
         data={"alias": "aaron-product", "related_product": "AaronProduct"},
     )
     expected = b'<?xml version="1.0" encoding="utf-8"?><success>Created/updated alias aaron-product</success>'
