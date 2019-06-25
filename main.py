0c99d480810929ed14561b1af06df8acbf4137d6
Aaron Amanuel <amanuel.aaron1@gmail.com>
2019-06-25 17:14:56 -0400

    port num can be change via env var

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import nazgul.api as api
 from flask import Flask
+import os
+
 
 if __name__ == "__main__":
     app = Flask(__name__, instance_relative_config=True)
     app.register_blueprint(api.bp)
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=os.environ.get("PORT", 5000))
