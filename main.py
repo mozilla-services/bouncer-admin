dca7780411cea5dc564d75e409883dbdae5bff78
Aaron Amanuel <amanuel.aaron1@gmail.com>
2019-06-26 17:32:26 -0400

    removed prefix for heartbeat endpoints

--- main.py
+++ main.py
@@ -6,4 +6,5 @@ import os
 if __name__ == "__main__":
     app = Flask(__name__, instance_relative_config=True)
     app.register_blueprint(api.bp)
+    app.register_blueprint(api.hb)
     app.run(host="0.0.0.0", port=os.environ.get("PORT", 5000))
