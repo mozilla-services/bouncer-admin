8d2e6ed65eecbbc87dd4509f7b53df2ba31becd3
Aaron Amanuel <amanuel.aaron1@gmail.com>
2019-06-27 11:09:09 -0400

    moved SECRECT KEY default in else branch

--- nazgul/__init__.py
+++ nazgul/__init__.py
@@ -6,7 +6,6 @@ from flask import Flask
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(SECRET_KEY="dev")
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -14,6 +13,7 @@ def create_app(test_config=None):
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
+        app.config.from_mapping(SECRET_KEY="dev")
 
     # ensure the instance folder exists
     try:
