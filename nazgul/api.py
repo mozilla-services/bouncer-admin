012a5b25ecd01737dee959090d7987525a094008
Aaron Amanuel <amanuel.aaron1@gmail.com>
2019-08-08 11:43:38 -0400

    used flask app config to enforce request size

--- nazgul/api.py
+++ nazgul/api.py
@@ -11,8 +11,6 @@ import os, time, logging, json
 bp = Blueprint("api", __name__, url_prefix="/api")
 hb = Blueprint("heartbeat", __name__)
 
-POST_SIZE_LIMIT = 500 * 1024
-
 auth = HTTPBasicAuth()
 
 usr_imp = json.loads(os.environ.get("AUTH_USERS", '{"admin":"admin"}'))
@@ -35,8 +33,11 @@ fh.setLevel(logging.DEBUG)
 logger.addHandler(fh)
 
 
-def verify_content_length(max_length):
-    return request.content_length and request.content_length <= max_length
+@bp.errorhandler(413)
+def request_entity_too_large(error):
+    xml = xmlrenderer.XMLRenderer()
+    data = xml.error("POST request length exceeded 500KB", errno=101)
+    return Response(data, mimetype="text/xml"), 413
 
 
 @auth.verify_password
@@ -107,10 +108,6 @@ def location_show():
 def location_add():
     xml = xmlrenderer.XMLRenderer()
 
-    if not verify_content_length(POST_SIZE_LIMIT):
-        data = xml.error("POST request length exceeded 500KB", errno=101)
-        return Response(data, mimetype="text/xml"), 400
-
     product = request.form.get("product", None)
     os = request.form.get("os", None)
     path = request.form.get("path", None)
@@ -142,10 +139,6 @@ def location_add():
 def location_modify():
     xml = xmlrenderer.XMLRenderer()
 
-    if not verify_content_length(POST_SIZE_LIMIT):
-        data = xml.error("POST request length exceeded 500KB", errno=101)
-        return Response(data, mimetype="text/xml"), 400
-
     product = request.form.get("product", None)
     os = request.form.get("os", None)
     path = request.form.get("path", None)
@@ -170,10 +163,6 @@ def location_modify():
 @auth.login_required
 def location_delete():
     xml = xmlrenderer.XMLRenderer()
-
-    if not verify_content_length(POST_SIZE_LIMIT):
-        data = xml.error("POST request length exceeded 500KB", errno=101)
-        return Response(data, mimetype="text/xml"), 400
 
     location_id = request.form.get("location_id", None)
     if not location_id:
@@ -217,10 +206,6 @@ def product_show():
 def product_add():
     xml = xmlrenderer.XMLRenderer()
 
-    if not verify_content_length(POST_SIZE_LIMIT):
-        data = xml.error("POST request length exceeded 500KB", errno=101)
-        return Response(data, mimetype="text/xml"), 400
-
     product = request.form.get("product", None)
     languages = request.form.getlist("languages", None)
     ssl_only = request.form.get("ssl_only", "").lower() == "true"
@@ -243,10 +228,6 @@ def product_add():
 @auth.login_required
 def product_delete():
     xml = xmlrenderer.XMLRenderer()
-
-    if not verify_content_length(POST_SIZE_LIMIT):
-        data = xml.error("POST request length exceeded 500KB", errno=101)
-        return Response(data, mimetype="text/xml"), 400
 
     product = request.form.get("product", None)
     product_id = request.form.get("product_id", None)
@@ -273,10 +254,6 @@ def product_delete():
 def product_language_add():
     xml = xmlrenderer.XMLRenderer()
 
-    if not verify_content_length(POST_SIZE_LIMIT):
-        data = xml.error("POST request length exceeded 500KB", errno=101)
-        return Response(data, mimetype="text/xml"), 400
-
     product = request.form.get("product", None)
     languages = request.form.getlist("languages", None)
     try:
@@ -297,10 +274,6 @@ def product_language_add():
 @auth.login_required
 def product_language_delete():
     xml = xmlrenderer.XMLRenderer()
-
-    if not verify_content_length(POST_SIZE_LIMIT):
-        data = xml.error("POST request length exceeded 500KB", errno=101)
-        return Response(data, mimetype="text/xml"), 400
 
     product = request.form.get("product", None)
     languages = request.form.getlist("languages", None)
@@ -359,10 +332,6 @@ def uptake():
 @auth.login_required
 def create_update_alias():
     xml = xmlrenderer.XMLRenderer()
-
-    if not verify_content_length(POST_SIZE_LIMIT):
-        data = xml.error("POST request length exceeded 500KB", errno=101)
-        return Response(data, mimetype="text/xml"), 400
 
     alias = request.form.get("alias", None)
     related_product = request.form.get("related_product", None)
