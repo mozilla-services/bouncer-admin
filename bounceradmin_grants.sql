ad4fd1cf4637ff679e612de75f525fbaa07bf6dd
Aaron Amanuel <amanuel.aaron1@gmail.com>
2019-08-13 11:09:43 -0400

    added list of commands for establishing bounceradmin privileges

---
+++ bounceradmin_grants.sql
@@ -0,0 +1,9 @@
+use bouncer;
+revoke all privileges on bouncer.* from 'bounceradmin'@'%';
+grant SELECT,INSERT,UPDATE,DELETE ON mirror_os TO ‘bounceradmin’@‘%’;
+grant SELECT,INSERT,UPDATE,DELETE ON mirror_locations TO ‘bounceradmin’@‘%’;
+grant SELECT,INSERT,UPDATE,DELETE ON mirror_products TO ‘bounceradmin’@‘%’;
+grant SELECT,INSERT,UPDATE,DELETE ON mirror_product_langs TO ‘bounceradmin’@‘%’;
+grant SELECT,INSERT,UPDATE,DELETE ON mirror_mirrors TO ‘bounceradmin’@‘%’;
+grant SELECT,INSERT,UPDATE,DELETE ON mirror_aliases TO ‘bounceradmin’@‘%’;
+flush privileges;
